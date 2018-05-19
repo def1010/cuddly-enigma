a95fabbdfa6e685c81a122a246e7258e1d2a8332
def1010 <edbarrett@gmail.com>
2018-05-19 10:17:20 +1000

    new

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import application
-
+from app.forms import LoginForm
 
 @application.route('/')
 @application.route('/index')
@@ -17,3 +17,8 @@ def index():
         }
     ]
     return render_template('index.html',title='Home', user=user, posts=posts)
+
+@application.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
